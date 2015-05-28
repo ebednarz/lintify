f6217b2dec80f2db1b0aa1d206e11c78ca66d89f
Eric Bednarz <github@bednarz.nl>
2015-05-28 23:39:48 +0200

    Fix guard

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 var bufferFactory = require('./library/bufferFactory');
+var isPackageFile = require('is-package-file');
 var path = require('path');
 var through2 = require('through2');
 
 function lintify(modulePath, options) {
     var filePath = path.relative(process.cwd(), modulePath);
-    var stream = through2(bufferFactory(filePath, options));
+    var stream = isPackageFile(modulePath) ?
+        through2() :
+        through2(bufferFactory(filePath, options));
     return stream;
 }
 
